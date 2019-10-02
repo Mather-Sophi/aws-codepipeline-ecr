9ead50498229f8839e7e4a863077c07c8fa3daf1
Steve Mickeler <smickeler@globeandmail.com>
2019-10-02 19:16:41 -0400

    Update codebuild module source

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_iam_role_policy" "codepipeline_baseline" {
 }
 
 module "codebuild_project" {
-  source = "../aws-codebuild-project"
+  source = "github.com/globeandmail/aws-codebuild-project?ref=1.0"
 
   name        = var.name
   deploy_type = ecr
